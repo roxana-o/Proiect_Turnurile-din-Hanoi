006133f4bed9139278f6d7a2847b949a33e723f2
roxana-o <120491557+roxana-o@users.noreply.github.com>
2022-12-19 16:06:47 +0000

    Commit2

--- proiect.c
+++ proiect.c
@@ -16,16 +16,74 @@ void Turnuri_Hanoi(char sursa, char destinatie, char auxiliar, int n)
 	}
 }
 
+void Informatii() {
+	printf("\n Turnurile din Hanoi \n");
+	printf(" * Joc matematic\n ");
+	printf("* Inventat de matematicianul francez Edouard Lucas în anul 1883 \n ");
+	printf("* Se dau 3 tije simbolizate prin A,B,C. Pe tija A se gasesc discuri de diametre diferite,\n");
+	printf("asezate in ordine descrescatoare a diametrelor privite de jos in sus. Se cere sa se mute \n");
+	printf("discurile de pe tija A pe tija C, utilizand ca tija intermediara tija B si respectand anumite reguli. \n");;
+}
+
+
+void Meniu()
+{
+	int optiune, n;
+	
+	do {
+		
+		printf("\n MENIU:\n");
+		printf("1. Ce presupune problema si cine a inventat-o? \n");
+		printf("2. Reguli reguli reguli\n");		
+		printf("3. Legenda din spatele problemei\n");
+		printf("4. Care e totusi solutia problemei? \n");
+		printf("0. Iesire \n");
+
+		printf("Alegeti o optiune din MENIU: ");
+		scanf("%d", &optiune);
+
+		switch (optiune)
+		{
+		case 1:
+			Informatii();
+			
+			break;
+
+		case 2:
+			
+			
+			break;
+
+		case 3:
+		
+		
+			break;
+
+		case 4:
+
+			printf("Alegeti numarul de discuri: \n");
+			scanf("%d", &n);
+
+			printf("\n Solutia pentru %d discuri este: \n", n);
+			Turnuri_Hanoi('A', 'B', 'C', n);
+			printf("\n Felicitari! Discurile au fost mutate de pe tija A pe tija C. \n");
+
+			
+			break;
+
+		case 0:
+			exit(1);
+
+		default:
+			printf("\nOptiunea nu este valabila. Alegeti din MENIU!\n");
+			break;
+		}
+
+	} while (1);
+}
+
 	int main() {
 
-		int n;
-
-		printf("Alegeti numarul de discuri: \n");
-		scanf("%d", &n);
-
-		printf("\n Solutia pentru %d discuri este: \n", n);
-		Turnuri_Hanoi('A', 'B', 'C', n);
-		printf("\n Felicitari! Discurile au fost mutate de pe tija A pe tija C. \n");
-
+		Meniu();
 		return 0;
 	}
